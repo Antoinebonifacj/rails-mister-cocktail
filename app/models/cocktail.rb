8a50bc7c1843a9665015257cd6a3672db75ec888
Antoine Bonifacj <abonifacj63@gmail.com>
2020-02-20 11:43:57 -0500

    finishing models again

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
-  has_many :doses
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true
